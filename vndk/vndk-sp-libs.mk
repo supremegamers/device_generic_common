ec1647df51898b05d2ad6b164cf9bee39a33a5b0
Logan Chien <loganchien@google.com>
2017-06-22 19:30:51 +0800

    vndk-sp: Remove android.hidl.memory@1.0-impl
    This commit removes android.hidl.memory@1.0-impl.so from
    /system/lib[64]/vndk-sp becuase android.hidl.memory@1.0.so is using
    absolute path and will only search for /system/lib[64]/hw or
    /vendor/lib[64]/hw.
    
    Bug: 35107583
    Bug: 62778280
    Test: sailfish boots
    Test: Write a small SP-HAL which links to libhidlmemory, load the
    testing SP-HAL lib from system executable, and it works when
    ld.config.txt exposes android.hidl.memory@1.0-impl.so from default
    linker namespace.
    
    Merged-In: I87238337cb70ff9b27ef1b25dc5fb982a84ff0b3
    Merged-In: I8e33c560a9ce62403aa7102eeb0a8bcd78116f84
    Change-Id: If4a9834c27b532ca8db24d497281ab588b4715f5
    (cherry picked from commit 490c47fc14a07830548c6c4465a42abbcc879034)
    (cherry picked from commit cbcac22895bb51cc6e1c8e13a43263ff333a2f88)

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -5,7 +5,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.renderscript@1.0 \
     android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
-    android.hidl.memory@1.0-impl \
     libRSCpuRef \
     libRSDriver \
     libRS_internal \
