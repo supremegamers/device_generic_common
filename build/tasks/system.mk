ea8e9aee2162059c71b6f50bdef56d0b51eef522
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-10-31 17:40:05 +0800

    build: remove two properties from build.prop
    Don't set ro.product.manufacturer and ro.product.model
    in build.prop. We will set them at runtime.

--- build/tasks/system.mk
+++ build/tasks/system.mk
@@ -1,3 +1,8 @@
+$(PRODUCT_OUT)/build.prop: $(INSTALLED_BUILD_PROP_TARGET)
+	sed -E '/ro.product.manufacturer|ro.product.model/d' $< > $@ && cat $@ > $<
+
+$(BUILT_SYSTEMIMAGE): $(PRODUCT_OUT)/build.prop
+
 ifneq ($(MKSQUASHFS),)
 $(PRODUCT_OUT)/system.sfs: $(BUILT_SYSTEMIMAGE) | $(SIMG2IMG)
 	$(hide) $(SIMG2IMG) $< $@
