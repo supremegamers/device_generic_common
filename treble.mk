79270bf32d554a99411666f14c3dab36de558416
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-11-10 13:47:37 +0800

    Binderize the audio HAL

--- treble.mk
+++ treble.mk
@@ -11,6 +11,7 @@ PRODUCT_PACKAGES += \
 # Audio HAL
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
