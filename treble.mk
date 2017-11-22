aed44f9ad4e9093c367634d3d9e5dcb37474209c
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-11-22 16:42:16 +0800

    Add HIDL HAL for power
    Set android.hardware.power@1.0 to binderized.

--- treble.mk
+++ treble.mk
@@ -52,7 +52,8 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
