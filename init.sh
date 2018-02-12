e558a018429d50ca7d787165b79b9c486f24c414
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-02-12 15:13:36 +0800

    init.sh: adjust micphone volume
    Change the values according to internal testing.
    
    Tested-by: 张淋苹 <18701158269@163.com>

--- init.sh
+++ init.sh
@@ -434,13 +434,13 @@ function do_bootcomplete()
 			alsa_amixer -c $c set Headphone on
 			alsa_amixer -c $c set Headphone 100%
 			alsa_amixer -c $c set Speaker 100%
-			alsa_amixer -c $c set Capture 100%
+			alsa_amixer -c $c set Capture 80%
 			alsa_amixer -c $c set Capture cap
 			alsa_amixer -c $c set PCM 100 unmute
 			alsa_amixer -c $c set SPO unmute
 			alsa_amixer -c $c set IEC958 on
-			alsa_amixer -c $c set 'Mic Boost' 3
-			alsa_amixer -c $c set 'Internal Mic Boost' 3
+			alsa_amixer -c $c set 'Mic Boost' 1
+			alsa_amixer -c $c set 'Internal Mic Boost' 1
 		fi
 	done
 
