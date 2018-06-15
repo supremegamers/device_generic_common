supremegamers/device_generic_common

commit 66fd14a317cbada5d7f05482504788839b26d9bf

Always use 64-bit binder

BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_EXTRA_KERNEL_MODULES := tp_smapi
 ifneq ($(filter efi_img,$(MAKECMDGOALS)),)
 TARGET_KERNEL_ARCH ?= x86_64
 endif
-TARGET_USES_64_BIT_BINDER := $(if $(filter x86_64,$(TARGET_ARCH) $(TARGET_KERNEL_ARCH)),true)
+TARGET_USES_64_BIT_BINDER := true
 
 BOARD_USES_GENERIC_AUDIO ?= false
 BOARD_USES_ALSA_AUDIO ?= true

commit bb48217acd435cc404bd75e3ab2cce5bc3dce122

Fix issues to use 64-bit binder on 32-bit image
Fixes: 66fd14a3 (Always use 64-bit binder)

Reported-and-Tested-by: Mauro Rossi <issor.oruam@gmail.com>

BoardConfig.mk
@@ -76,7 +76,7 @@ ifneq ($(strip $(BOARD_GPU_DRIVERS)),)
 TARGET_HARDWARE_3D := true
 endif
 
-BOARD_KERNEL_CMDLINE := root=/dev/ram0 androidboot.selinux=permissive $(if $(filter true,$(TARGET_USES_64_BIT_BINDER)),,vmalloc=192M)
+BOARD_KERNEL_CMDLINE := root=/dev/ram0 androidboot.selinux=permissive $(if $(filter x86_64,$(TARGET_ARCH) $(TARGET_KERNEL_ARCH)),,vmalloc=192M)
 TARGET_KERNEL_DIFFCONFIG := device/generic/common/selinux_diffconfig
 
 COMPATIBILITY_ENHANCEMENT_PACKAGE := true

selinux_diffconfig
@@ -5,3 +5,4 @@ CONFIG_SECURITY_SELINUX_CHECKREQPROT_VALUE=1
 CONFIG_DEFAULT_SECURITY_SELINUX=y
 # CONFIG_DEFAULT_SECURITY_DAC is not set
 CONFIG_DEFAULT_SECURITY="selinux"
+# CONFIG_ANDROID_BINDER_IPC_32BIT is not set