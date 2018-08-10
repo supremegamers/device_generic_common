2a143a7c18a102af2ade8b36f681170e0e5acc48
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-08-10 10:40:46 +0800

    init.sh: fix Bluetooth for ASUS T100TAF

--- init.sh
+++ init.sh
@@ -55,6 +55,9 @@ function init_hal_bluetooth()
 	done
 
 	case "$PRODUCT" in
+		T100TAF)
+			set_property bluetooth.interface hci1
+			;;
 		T10*TA|M80TA|HP*Omni*)
 			BTUART_PORT=/dev/ttyS1
 			set_property hal.bluetooth.uart.proto bcm
