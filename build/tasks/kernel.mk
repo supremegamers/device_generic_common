supremegamers/device_generic_common

commit 9ea487d8af2edba3df1330a8a48f91412289a375

kernel.mk: set LC_CTYPE to C locale
The simple workaround avoids AOSP's prebuilt flex crashing like

flex-2.5.39: loadlocale.c:130: _nl_intern_locale_data: Assertion `cnt < (sizeof (_nl_value_type_LC_TIME) / sizeof (_nl_value_type_LC_TIME[0]))' failed.
Aborted (core dumped)

build/tasks/kernel.mk
@@ -8,6 +8,8 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 
+export LC_CTYPE := C
+
 ifneq ($(TARGET_NO_KERNEL),true)
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 
@@ -71,7 +73,7 @@ KERNEL_MODULES_DEP := $(firstword $(wildcard $(TARGET_OUT)/lib/modules/*/modules
 KERNEL_MODULES_DEP := $(if $(KERNEL_MODULES_DEP),$(KERNEL_MODULES_DEP),$(TARGET_OUT)/lib/modules)
 
 ALL_EXTRA_MODULES := $(patsubst %,$(TARGET_OUT_INTERMEDIATES)/kmodule/%,$(TARGET_EXTRA_KERNEL_MODULES))
-$(ALL_EXTRA_MODULES): $(TARGET_OUT_INTERMEDIATES)/kmodule/%: $(BUILT_KERNEL_TARGET)
+$(ALL_EXTRA_MODULES): $(TARGET_OUT_INTERMEDIATES)/kmodule/%: $(BUILT_KERNEL_TARGET) | $(ACP)
 	@echo Building additional kernel module $*
 	$(hide) mkdir -p $(@D) && $(ACP) -fr $(EXTRA_KERNEL_MODULE_PATH_$*) $(@D)
 	$(mk_kernel) M=$(abspath $@) modules

commit a07fe43946960448c90632e69852f6f78094ab57

kernel.mk: set LC_MESSAGES to C locale
The error messages are unreadable since commit 9ea487d changed LC_CTYPE
to C locale but left LC_MESSAGES unchanged. Change LC_MESSAGES as well
to fix it.

Fixes: 9ea487d (kernel.mk: set LC_CTYPE to C locale)

build/tasks/kernel.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014-2017 The Android-x86 Open Source Project
+# Copyright (C) 2014-2019 The Android-x86 Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,6 +9,7 @@
 #
 
 export LC_CTYPE := C
+export LC_MESSAGES := C
 
 ifneq ($(TARGET_NO_KERNEL),true)
 ifeq ($(TARGET_PREBUILT_KERNEL),)