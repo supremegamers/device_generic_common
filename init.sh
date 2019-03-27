444d32b416236364efbc3ab1b73d767bbc6de266
Chih-Wei Huang <cwhuang@linux.org.tw>
2019-03-27 18:08:19 +0800

    init.sh: invert axes of sensors for Microtech e-tab Pro
    With panel-orientation-quirks applied in i915 driver, the display is
    rotated 180 degrees. We need to invert the x and y axes of sensors
    as well.

--- init.sh
+++ init.sh
@@ -270,9 +270,6 @@ function init_hal_sensors()
 			modprobe hdaps
 			hal_sensors=hdaps
 			;;
-		*e-tabPro*)
-			set_property ro.iio.accel.quirks no-trig
-			;&
 		*i7Stylus*|*M80TA*)
 			set_property ro.iio.accel.x.opt_scale -1
 			;;
@@ -284,7 +281,7 @@ function init_hal_sensors()
 		*ST70416-6*)
 			set_property ro.iio.accel.order 102
 			;;
-		*pnEZpad*)
+		*e-tabPro*|*pnEZpad*)
 			set_property ro.iio.accel.quirks no-trig
 			;&
 		*T*0*TA*)
