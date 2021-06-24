ee35a2bfc51bd5a162d8d1778a1fb982ecdd01cd
Jon West <electrikjesus@gmail.com>
2021-06-24 18:16:56 -0400

    Update manifest for Bluetooth 1.1

--- treble.mk
+++ treble.mk
@@ -17,7 +17,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service.btlinux
+    android.hardware.bluetooth@1.1-service.btlinux \
+    android.hardware.bluetooth.audio@2.1-impl
 
 # Camera HAL
 PRODUCT_PACKAGES += \
