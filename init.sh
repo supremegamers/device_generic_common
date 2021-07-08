8a1eeb9119c2e88b1d4a2820018e87c034c038fb
Chih-Wei Huang <cwhuang@linux.org.tw>
2021-07-08 22:01:13 +0800

    init.sh: set ro.serialno from the DMI table

--- init.sh
+++ init.sh
@@ -29,6 +29,7 @@ function init_misc()
 	setprop ro.product.manufacturer "$(cat $DMIPATH/board_vendor)"
 	if [ -z "$VENDOR" ]; then setprop ro.product.brand "$(cat $DMIPATH/board_vendor)"; else setprop ro.product.brand "$VENDOR"; fi
 	if [ -z "$PRODUCT" ]; then setprop ro.product.model "$BOARD"; else setprop ro.product.model "$PRODUCT"; fi
+	setprop ro.serialno "$(cat $DMIPATH/product_serial)"
 
 	# a hack for USB modem
 	lsusb | grep 1a8d:1000 && eject
