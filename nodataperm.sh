476098fcf91d26ac4d1271c68a4d44361950e0ab
hmtheboy154 <buingoc67@gmail.com>
2022-08-10 01:44:52 +0700

    Revert "HACK: granting full permission for apps data (1/2)"
    This reverts commit 2d47891376c96011b2ee3c1ccef61cb48e15aed6.

--- nodataperm.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-chmod 777 -R /sdcard/Android
-chmod 777 -R /data/media/0/Android 
-chmod 777 -R /sdcard/Android/data
-chmod 777 -R /data/media/0/Android/obb 
-chmod 777 -R /mnt/*/*/*/*/Android/data
-chmod 777 -R /mnt/*/*/*/*/Android/obb
