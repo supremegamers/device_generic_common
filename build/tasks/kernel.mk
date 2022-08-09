497cb2c17973582ee1d878d44c48c968240507ea
hmtheboy154 <buingoc67@gmail.com>
2022-08-10 01:53:10 +0700

    kernel.mk : Use clang-r450784 (14.0.3)
    Hosted in crDroid's Gitlab : https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r450784

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -8,7 +8,7 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 
-TARGET_CLANG_PATH := prebuilts/clang/host/linux-x86/clang-latest/bin
+TARGET_CLANG_PATH := prebuilts/clang/host/linux-x86/clang-r450784/bin
 
 ifneq ($(TARGET_NO_KERNEL),true)
 ifeq ($(TARGET_PREBUILT_KERNEL),)
