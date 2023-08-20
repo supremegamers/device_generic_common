77c42bf0305157bd3d696ef78a91a2fb4513e453
hmtheboy154 <buingoc67@gmail.com>
2023-08-20 03:11:05 -0400

    Do not build and use libcamera yet
    uvcvideo pipeline need some format conversion, so we should use this
    HAL for surface builds instead for IPU3
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- glodroid/device_glodroid.mk
+++ glodroid/device_glodroid.mk
@@ -12,12 +12,14 @@ PRODUCT_PACKAGES += \
 #    $(LOCAL_PATH)/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
 #Camera HAL
+ifeq ($(BOARD_BUILD_AOSPEXT_LIBCAMERA),true)
 PRODUCT_PACKAGES += \
     ipa_ipu3.so ipa_ipu3.so.sign \
     camera.libcamera libcamera libcamera-base libcamera-cam lc-compliance \
     android.hardware.camera.provider@2.5-service_64 libdav1d dav1d \
 
 PRODUCT_PROPERTY_OVERRIDES += ro.hardware.camera=libcamera
+endif
 
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.concurrent.xml \
@@ -25,3 +27,4 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.camera.external.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.external.xml \
