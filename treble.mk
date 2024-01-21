6edde801d34e33cf98989db26561bc09f9d999b6
hmtheboy154 <buingoc67@gmail.com>
2024-01-21 14:52:36 -0500

    Cleaning android.hardware.drm
    Remove version 1.0, we don't need that anyway
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- treble.mk
+++ treble.mk
@@ -92,8 +92,6 @@ PRODUCT_PACKAGES += \
 
 # Drm HAL
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.4-service-lazy.clearkey
 
 # GPS HAL
