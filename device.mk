a4dc2033787a361118c45b7ac943f5c767976425
hmtheboy154 <buingoc67@gmail.com>
2024-04-09 14:46:22 -0400

    Temporary putting a script to modprobe at early-init stage
    Due to the fact that we are forced to use updatable APEX, uevent modprobe
    started to become unreliable as libc may not be prepared yet. Temporary
    make a modprobe script using toybox modprobe and run it at early-init.
    
    This is not the definitive solution, we need to find a better way.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ endif
 PRODUCT_COPY_FILES := \
     $(if $(wildcard $(PRODUCT_DIR)init.rc),$(PRODUCT_DIR)init.rc:root/init.rc) \
     $(if $(wildcard $(PRODUCT_DIR)init.sh),$(PRODUCT_DIR),$(LOCAL_PATH)/)init.sh:system/etc/init.sh \
+    $(if $(wildcard $(PRODUCT_DIR)init.modprobe.sh),$(PRODUCT_DIR),$(LOCAL_PATH)/)init.modprobe.sh:$(TARGET_COPY_OUT_VENDOR)/etc/init.modprobe.sh \
     $(if $(wildcard $(PRODUCT_DIR)modules.blocklist),$(PRODUCT_DIR),$(LOCAL_PATH)/)modules.blocklist:system/etc/modules.blocklist \
     $(if $(wildcard $(PRODUCT_DIR)modules.options),$(PRODUCT_DIR),$(LOCAL_PATH)/)modules.options:system/etc/modules.options \
     $(if $(wildcard $(PRODUCT_DIR)fstab.$(TARGET_PRODUCT)),$(PRODUCT_DIR)fstab.$(TARGET_PRODUCT),$(LOCAL_PATH)/fstab.x86):root/fstab.$(TARGET_PRODUCT) \
